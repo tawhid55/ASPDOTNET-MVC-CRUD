030d1f1d480787f6af86bd5bbe67f74ee6b5a069
Tawhid Hasan <tawhidhasan44@gmail.com>
2023-07-09 03:49:02 +0600

    Updated

---
+++ ASPDOTNET-MVC-CRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPDOTNET_MVC_CRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+    }
+}
