9c2916ba5e9fff77c711e9c02ae3636d994fa0c3
Tawhid Hasan <tawhidhasan44@gmail.com>
2023-07-10 01:06:34 +0600

    Empolye add done and showing list done

--- ASPDOTNET-MVC-CRUD/Controllers/EmployeesController.cs
+++ ASPDOTNET-MVC-CRUD/Controllers/EmployeesController.cs
@@ -1,13 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPDOTNET_MVC_CRUD.Data;
+using ASPDOTNET_MVC_CRUD.Models;
+using ASPDOTNET_MVC_CRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPDOTNET_MVC_CRUD.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly DemoDbContext demoDbContext;
+
+        public EmployeesController(DemoDbContext demoDbContext)
+        {
+            this.demoDbContext = demoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await demoDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employe = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+            };
+
+            await demoDbContext.Employees.AddAsync(employe);
+            await demoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
 
 
