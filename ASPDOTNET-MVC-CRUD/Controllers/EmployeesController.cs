31585bdbefa1dea81095ffc73581e886ea8376ee
Tawhid Hasan <tawhidhasan44@gmail.com>
2023-07-18 22:57:35 +0600

    Added the update and delete functionality and the view

--- ASPDOTNET-MVC-CRUD/Controllers/EmployeesController.cs
+++ ASPDOTNET-MVC-CRUD/Controllers/EmployeesController.cs
@@ -45,6 +45,67 @@ namespace ASPDOTNET_MVC_CRUD.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employe = await demoDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
 
+            if(employe != null)
+            {
+                var viewModel = new UpdateEmployeeViewModel()
+                {
+                    Id = employe.Id,
+                    Name = employe.Name,
+                    Email = employe.Email,
+                    Salary = employe.Salary,
+                    Department = employe.Department,
+                    DateOfBirth = employe.DateOfBirth
+                };
+
+                return await Task.Run(() => View("View", viewModel));
+            }
+
+            
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateEmployeeViewModel updateEmployeeViewModel)
+        {
+            var employee = await demoDbContext.Employees.FindAsync(updateEmployeeViewModel.Id);
+            
+            if(employee != null)
+            {
+                employee.Name = updateEmployeeViewModel.Name;
+                employee.Email = updateEmployeeViewModel.Email;
+                employee.Salary = updateEmployeeViewModel.Salary;
+                employee.DateOfBirth = updateEmployeeViewModel.DateOfBirth;
+                employee.Department = updateEmployeeViewModel.Department;
+
+                await demoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+                
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateEmployeeViewModel updateEmployeeViewModel)
+        {
+            var employee = await demoDbContext.Employees.FindAsync(updateEmployeeViewModel.Id);
+
+            if(employee != null)
+            {
+                demoDbContext.Employees.Remove(employee);
+                await demoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
